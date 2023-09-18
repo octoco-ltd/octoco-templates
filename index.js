5229229dec8ae5a03552457d3233d6a2c64cb260
James de Villiers <james@octoco.ltd>
2023-09-18 11:19:40 +0200

    init

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 #!/usr/bin/env node
 
-const {spawn} = require('cross-spawn')
-const fs = require('fs')
-const path = require('path')
-const {welcomeFlow} = require("./welcome-flow");
-const {frontendFlow} = require("./frontend-flow");
-const {backendFlow} = require("./backend-flow");
-const {singleRepoFlow} = require("./single-repo-flow");
-const {dirFlow} = require("./dir-flow");
+// const {spawn} = require('cross-spawn')
+// const fs = require('fs')
+// const path = require('path')
+// const {welcomeFlow} = require("./welcome-flow");
+// const {frontendFlow} = require("./frontend-flow");
+// const {backendFlow} = require("./backend-flow");
+// const {singleRepoFlow} = require("./single-repo-flow");
+// const {dirFlow} = require("./dir-flow");
+
+import {welcomeFlow} from "./welcome-flow.js";
 
 
 const server = async function () {
@@ -15,61 +17,62 @@ const server = async function () {
     let backend = null;
 
     const isMonorepo = await welcomeFlow();
-    if (isMonorepo) {
-        frontend = await frontendFlow();
-        backend = await backendFlow();
-    } else {
-        const isFrontendApp = await singleRepoFlow();
-        if (isFrontendApp) {
-            frontend = await frontendFlow();
-        } else {
-            backend = await backendFlow();
-        }
-    }
-
-
-    const projectName = await dirFlow()
-
-
-// Create a project directory with the project name.
-    const currentDir = process.cwd();
-    const projectDir = path.resolve(currentDir, projectName);
-    fs.mkdirSync(projectDir, {recursive: true});
-
-// A common approach to building a starter template is to
-// create a `template` folder which will house the template
-// and the files we want to create.
-    const templateDir = path.resolve(__dirname, 'templates/express-rest-api');
-    fs.cpSync(templateDir, projectDir, {recursive: true});
-
-// It is good practice to have dotfiles stored in the
-// template without the dot (so they do not get picked
-// up by the starter template repository). We can rename
-// the dotfiles after we have copied them over to the
-// new project directory.
-    fs.renameSync(
-        path.join(projectDir, 'gitignore'),
-        path.join(projectDir, '.gitignore')
-    );
-
-    const projectPackageJson = require(path.join(projectDir, 'package.json'));
-
-// Update the project's package.json with the new project name
-    projectPackageJson.name = projectName;
-
-    fs.writeFileSync(
-        path.join(projectDir, 'package.json'),
-        JSON.stringify(projectPackageJson, null, 2)
-    );
-
-// Run `npm install` in the project directory to install
-// the dependencies. We are using a third-party library
-// called `cross-spawn` for cross-platform support.
-// (Node has issues spawning child processes in Windows).
-    spawn.sync('npm', ['install'], {stdio: 'inherit'});
-
-    console.log('Success! Your new project is ready.');
-    console.log(`Created ${projectName} at ${projectDir}`);
+    console.log(isMonorepo)
+//     if (isMonorepo) {
+//         frontend = await frontendFlow();
+//         backend = await backendFlow();
+//     } else {
+//         const isFrontendApp = await singleRepoFlow();
+//         if (isFrontendApp) {
+//             frontend = await frontendFlow();
+//         } else {
+//             backend = await backendFlow();
+//         }
+//     }
+//
+//
+//     const projectName = await dirFlow()
+//
+//
+// // Create a project directory with the project name.
+//     const currentDir = process.cwd();
+//     const projectDir = path.resolve(currentDir, projectName);
+//     fs.mkdirSync(projectDir, {recursive: true});
+//
+// // A common approach to building a starter template is to
+// // create a `template` folder which will house the template
+// // and the files we want to create.
+//     const templateDir = path.resolve(__dirname, 'templates/express-rest-api');
+//     fs.cpSync(templateDir, projectDir, {recursive: true});
+//
+// // It is good practice to have dotfiles stored in the
+// // template without the dot (so they do not get picked
+// // up by the starter template repository). We can rename
+// // the dotfiles after we have copied them over to the
+// // new project directory.
+//     fs.renameSync(
+//         path.join(projectDir, 'gitignore'),
+//         path.join(projectDir, '.gitignore')
+//     );
+//
+//     const projectPackageJson = require(path.join(projectDir, 'package.json'));
+//
+// // Update the project's package.json with the new project name
+//     projectPackageJson.name = projectName;
+//
+//     fs.writeFileSync(
+//         path.join(projectDir, 'package.json'),
+//         JSON.stringify(projectPackageJson, null, 2)
+//     );
+//
+// // Run `npm install` in the project directory to install
+// // the dependencies. We are using a third-party library
+// // called `cross-spawn` for cross-platform support.
+// // (Node has issues spawning child processes in Windows).
+//     spawn.sync('npm', ['install'], {stdio: 'inherit'});
+//
+//     console.log('Success! Your new project is ready.');
+//     console.log(`Created ${projectName} at ${projectDir}`);
 
 };
 
