12d399e78bc314e690c205263c5be0714b24e6e1
OctoPikkie <christiaan@octoco.ltd>
2023-09-22 18:31:25 +0200

    na

--- templates/ts-react-app/src/components/avatar/__tests__/UserAvatar.cy.tsx
+++ templates/ts-react-app/src/components/avatar/__tests__/UserAvatar.cy.tsx
@@ -2,7 +2,6 @@ import UserAvatar from '../UserAvatar'
 
 describe('<UserAvatar />', () => {
   it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
     cy.mount(<UserAvatar />)
   })
 })
