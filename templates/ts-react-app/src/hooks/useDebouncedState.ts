e793bd525f0c25b303223ae02266c13a8cffdee2
Daniel Rossouw <daniel@rossouw.me>
2023-10-04 16:12:07 +0200

    debounced docs

--- templates/ts-react-app/src/hooks/useDebouncedState.ts
+++ templates/ts-react-app/src/hooks/useDebouncedState.ts
@@ -3,6 +3,14 @@ import { debounce } from 'lodash';
 
 type DebouncedStateSetter<T> = (value: T) => void;
 
+/**
+ * A hook that returns a debounced state value and a function to update it.
+ * @template T The type of the state value.
+ * @param initialValue The initial value of the state.
+ * @param delay The delay in milliseconds before updating the state.
+ * @returns A tuple containing the current state value and a function to update it.
+ * @example const [value, setValue] = useDebouncedState(0, 500);
+ */
 export default function useDebouncedState<T>(initialValue: T, delay = 500): [T, DebouncedStateSetter<T>] {
   const [state, setState] = useState<T>(initialValue);
 
