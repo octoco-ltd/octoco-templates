d9523a2beb72245a05ecf6762a1720f35df4d5dc
Emile Lombard <emile@octoco.ltd>
2024-05-26 15:51:40 +0200

    init turborepo and add backend for testing react query

---
+++ templates/ts-react-app-pwa/apps/backend/functions/src/index.ts
@@ -0,0 +1,41 @@
+/**
+ * 
+ */
+
+import { onRequest } from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
+import * as admin from 'firebase-admin';
+
+admin.initializeApp();
+
+const db = admin.firestore();
+
+export const helloWorld = onRequest(async (request: any, response: any) => {
+    try {
+        response.send("Hello from Firebase!");
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+export const addTodo = onRequest(async (request, response) => {
+    const { title, version, date, photoURL } = request.body;
+    const newTodo = { title, version, date, photoURL };
+
+    try {
+        const docRef = await db.collection('todos').add(newTodo);
+        response.status(201).send({ id: docRef.id, ...newTodo });
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+export const getTodos = onRequest(async (request, response) => {
+    try {
+        const snapshot = await db.collection('todos').get();
+        const todos = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        response.status(200).send(todos);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
