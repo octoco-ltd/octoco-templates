7eaad49a71ea9511ef40d63df51839b862c5011e
OctoPikkie <christiaan@octoco.ltd>
2024-05-27 19:37:05 +0200

    poc extra

--- templates/ts-react-app-pwa/apps/backend/functions/src/index.ts
+++ templates/ts-react-app-pwa/apps/backend/functions/src/index.ts
@@ -1,7 +1,3 @@
-/**
- * 
- */
-
 import { onRequest } from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 import * as admin from 'firebase-admin';
@@ -36,6 +32,32 @@ export const getTodos = onRequest(async (request, response) => {
         const snapshot = await db.collection('todos').get();
         const todos = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
         response.status(200).send(todos);
+    } catch (error) {
+        response.status(500).send(error);
+    }
+});
+
+
+export const addImage = onRequest(async (request, response) => {
+    const { title, photoURL } = request.body;
+
+    try {
+        // Save the file URL to Firestore
+        const newImage = { title, photoURL: photoURL };
+        const docRef = await db.collection('images').add(newImage);
+
+        response.status(201).send({ id: docRef.id, ...newImage });
+    } catch (error) {
+        console.log(`error!!, ${error}`);
+        response.status(500).send(error);
+    }
+});
+
+export const getImages = onRequest(async (request, response) => {
+    try {
+        const snapshot = await db.collection('images').get();
+        const images = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+        response.status(200).send(images);
     } catch (error) {
         response.status(500).send(error);
     }
